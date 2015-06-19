5317ff5839d7123580d829027615a3259d89c92a
Mike Pitre <michaelepitre@gmail.com>
2015-06-19 11:27:10 -0500

    migrated users and trips tables

--- db/migrate/20150619160438_create_trips.rb
+++ db/migrate/20150619160438_create_trips.rb
@@ -1,8 +1,8 @@
 class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
-      t.string :current_location
-      t.string :destination
+      t.string :current_location, null: false
+      t.string :destination, null: false
       t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps null: false
